27f318bc4433a0a3d9d5ad617b8502d2a4e4e9ce
Or Finberg <orff1989@gmail.com>
2024-02-03 19:57:48 +0200

    adding or code

---
+++ app/src/main/java/com/example/myapplication/MainCurrWork.java
@@ -0,0 +1,69 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class MainCurrWork extends AppCompatActivity {
+    private String startTime;
+    private String endTime;
+    private Button startingButton;
+    private Button endingButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_curr_work);
+
+        startingButton = findViewById(R.id.starting_button);
+        endingButton = findViewById(R.id.ending_button);
+        Button endShiftButton = findViewById(R.id.end_button);
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm", Locale.getDefault());
+        startTime = formatter.format(calendar.getTime());
+        endTime = "-";
+
+        startingButton.setText("Starting Time:\n" + startTime);
+        endingButton.setText("Ending Time:\n" + endTime);
+
+        startingButton.setOnClickListener(view -> showTimePickerDialog(true));
+        endingButton.setOnClickListener(view -> showTimePickerDialog(false));
+
+        endShiftButton.setOnClickListener(view -> {
+            if(endTime.equals("-")) {
+                endTime = formatter.format(calendar.getTime());
+            }
+            // TODO: Send startTime and endTime to the database
+
+//            Intent goToShifts = new Intent(MainCurrWork.this, MainShifts.class);
+//            goToShifts.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//            startActivity(goToShifts);
+        });
+    }
+
+    private void showTimePickerDialog(boolean isStartingTime) {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, (view, hourOfDay, minuteOfHour) -> {
+            String formattedTime = String.format(Locale.getDefault(), "%02d:%02d", hourOfDay, minuteOfHour);
+            if (isStartingTime) {
+                startTime = formattedTime;
+                startingButton.setText("Starting Time:\n" + formattedTime);
+            } else {
+                endTime = formattedTime;
+                endingButton.setText("Ending Time:\n" + formattedTime);
+            }
+        }, hour, minute, true);
+
+        timePickerDialog.show();
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainPreWork.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainPreWork extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_pre_work);
+
+        Button start_button = findViewById(R.id.start_button);
+        Button shift_button = findViewById(R.id.shift_button);
+        Button workmenu_button = findViewById(R.id.work_button);
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent curr_activity = new Intent(getApplicationContext(), MainCurrWork.class);
+                startActivity(curr_activity);
+            }
+        });
+
+        shift_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent curr_activity = new Intent(MainPreWork.this, MainShifts.class);
+//                startActivity(curr_activity);
+            }
+        });
+
+        workmenu_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent curr_activity = new Intent(MainPreWork.this, MainWorkMenu.class);
+//                startActivity(curr_activity);
+            }
+        });
+    }
+}
